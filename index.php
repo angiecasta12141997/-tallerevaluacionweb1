55da142aa39a7cf08e9475619e76d7010ec8672f
angiecastañeda <angieconcordia@gmail.com>
2020-06-12 21:15:34 -0500

    taller1de wep1archivo index.php
    ejercicios del 1 hasta el 6.

---
+++ index.php
@@ -0,0 +1,194 @@
+<?php
+
+//Punto numero 1 taller evaluativo momento uno WEB1.
+//Profesor JUAN JOSE GALLEGO MESA.
+//Presentado por ANGIE DANIELA CASTAÑEDA CASTAÑEDA.
+/**************************************/
+/*echo("calculadora aritmetica super eficiente");
+/**************************************/
+//Programado por ANGIE DANIELA CASTAÑEDA CASTAÑEDA.
+/*
+$primernum=20;
+$segundonum=80;
+
+$suma=$primernum+ $segundonum;
+$resta=$primernum-$segundonum;
+$multiplicacion=$primernum*$segundonum;
+$division=$segundonum/$primernum;
+echo("Super calculadora 2020 la mas eficiente de todas");
+echo("<br>");
+echo("la suma de: "."+".$segundonum ."=".$suma);
+echo("<br>");
+echo("la suma de: ".$primernum."-".$segundonum ." =".$resta);
+echo("<br>");
+echo("la suma de: ".$primernum."*".$segundonum."=".$multiplicacion);
+echo("<br>");
+echo("la suma de: ".$primernum."/".$segundonum."=".$division);
+ */
+/*/............................................................................../
+/............................................................................../*/
+
+//Punto numero 2 taller evaluativo momento uno WEB1.
+//Profesor JUAN JOSE GALLEGO MESA.
+//Presentado por ANGIE DANIELA CASTAÑEDA CASTAÑEDA.
+/*************************************************/
+/*echo("CALCULADORA DE IMC GIMNACIO BODYTECH");
+/************************************************/
+//Programado por ANGIE DANIELA CASTAÑEDA CASTAÑEDA.
+/*
+echo("<br>");
+$nombre="carla";
+$apellido="mejia";
+$edad=23;
+$cedula=1045648050;
+$direccion="cl82 #92-90";
+$peso=48; 
+$altura=1.60 ;
+/**********************************************************/
+/*$IMC=$peso/($altura*$altura);
+/**********************************************************/
+/*if($IMC<=18.4){
+    echo("Señor usuario,tiene insuficiencia de peso");
+    echo("<br>");
+    echo($IMC);
+
+}else if($IMC>=18.5 && $IMC<=24.9){
+    echo("Señor usuario, posee un peso normal");
+    echo("<br>");
+    echo($IMC);
+
+}else if($IMC>=25 && $IMC<=29.9){
+    echo("usted posee sobrepeso");
+    echo("<br>");
+    echo($IMC);
+}else if($IMC>=30 && $IMC<=34.9){
+    echo("Obesidad 1");
+    echo("<br>");
+    echo($IMC);
+}else if($IMC>=35 && $IMC<=39.9){
+
+    echo("usted posee Obesidad 2");
+    echo("<br>");
+    echo($IMC);
+
+}else if($IMC>=40){
+    echo("usted posee Obesidad 3");
+    echo("<br>");
+    echo($IMC);
+
+}
+
+
+/............................/
+echo($clasificacion1=IMC<=18.4("Insuficiencia de peso"));
+echo("<br>");
+echo($clasificacion2=18.5<=IMC<=24.9("Peso normal"));
+echo("<br>");
+echo($clasificacion3=25<=IMC<=29.9("Sobre peso"));
+echo("<br>");
+echo($clasificacion4=30<=IMC<=34.9("Obesidad 1"));
+echo("<br>");
+echo($clasificacion5=35<=IMC<=39.9("Obesidad 2"));
+echo("<br>");
+echo($clasificacion6=IMC>=40("Obesidad 3"));
+echo("<br>");
+echo("peso/altura * altura");
+
+echo("*********************");
+echo("<br>");
+echo("Gimnacio BODYTECH");
+echo("<br>");
+echo("Una experiencia unica para ti y tu salud");
+echo("<br>");
+echo("Cr83#52-33");
+echo("<br>");
+echo("Medellin-Colombia");
+echo("(4)2884355 - 3226522834");
+echo("*********************");
+/*
+/............................................................................../
+/............................................................................../*/
+
+//Punto numero 3 taller evaluativo momento uno WEB1.
+//Profesor JUAN JOSE GALLEGO MESA.
+//Presentado por ANGIE DANIELA CASTAÑEDA CASTAÑEDA.
+/*************************************************/
+/*echo("SPRING STEP");
+/************************************************/
+//Programado por ANGIE DANIELA CASTAÑEDA CASTAÑEDA.
+/*
+echo("<br>");
+$cantidadZapatos = 5;
+$costoneto = 150000;
+$totalCompra = ($cantidadZapatos * $costoneto) ;
+$valordescuento=0.20 ;
+
+if ( $cantidadZapatos == 3 ) {
+  $valordescuento = $totalCompra * 0.10 ;
+    
+} else if ( $cantidadZapatos > 3 && $cantidadZapatos <= 5 ) {
+  $valordescuento = $totalCompra * 0.20 ;
+    
+} else  if ( $cantidadZapatos > 5 ) {
+  $valordescuento = $totalCompra * 0.40 ;
+}
+$valorPagar = ($cantidadZapatos * $costoneto)-$valordescuento;
+
+echo("Cantidad Vendida:"  .  $cantidadZapatos  .    " Costo Neto:"  .  $totalCompra  .    
+"Valor del Descuento:"  .  $valordescuento  .    " Valor a Pagar es:"  .  $valorPagar );
+/*
+/............................................................................../
+/............................................................................../
+
+//Punto numero 4 taller evaluativo momento uno WEB1.
+//Profesor JUAN JOSE GALLEGO MESA.
+//Presentado por ANGIE DANIELA CASTAÑEDA CASTAÑEDA.
+/*************************************************/
+/*echo("EMPLEADO POSTOBON");
+/************************************************/
+//Programado por ANGIE DANIELA CASTAÑEDA CASTAÑEDA.
+/*
+echo("<br>");
+$sueldohora=20000;
+$horasextras=25000;
+$horastrabajadas=36;
+$sueldosemanal=($horastrabajadas * $sueldohora);
+
+if($horastrabajadas<= 40){
+    $sueldohora=$sueldohora;
+} else if ($sueldohora>40){
+    $horasextras=$horasextras;
+}
+$sueldo=($horastrabajadas * $sueldohora);
+echo($sueldo);
+/*
+/............................................................................../
+/............................................................................../
+
+//Punto numero 5 taller evaluativo momento uno WEB1.
+//Profesor JUAN JOSE GALLEGO MESA.
+//Presentado por ANGIE DANIELA CASTAÑEDA CASTAÑEDA.
+/*************************************************/
+echo("FRUTERIA Y HELADERIA LA JUGOSA SAS");
+/************************************************/
+//Consulta echa por ANGIE DANIELA CASTAÑEDA CASTAÑEDA.
+
+$salpicon=array(
+"dulce1"=>"Banano",
+"dulce2"=>"Manzana",
+"dulce3"=>"Durazno",
+"acido1"=>"Piña",
+"acido2"=>"Naranja",
+"acido3"=>"Lulo"
+);
+echo("<br>");
+echo("<br>");
+foreach($salpicon as $clave=>$valor){
+    echo($clave);
+    echo("=>");
+    echo($valor);
+    echo("<br>");
+}
+
+
+?>
